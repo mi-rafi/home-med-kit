61dcd7cbfd083de9f541cc63699311b6f9732615
kasim <kasimovara@mail.ru>
2022-06-30 23:54:09 +0300

    Set up bot API

--- src/main/java/com/github/mirafi/homemedkit/config/TelegramApiConfiguration.java
+++ src/main/java/com/github/mirafi/homemedkit/config/TelegramApiConfiguration.java
@@ -1,2 +1,24 @@
-package com.github.mirafi.homemedkit.config;public class TelegramApiConfiguration {
+package com.github.mirafi.homemedkit.config;
+
+
+import com.github.mirafi.homemedkit.controller.BotController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class TelegramApiConfiguration {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(LongPollingBot bot) throws TelegramApiException {
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(bot);
+        return telegramBotsApi;
+    }
+
 }
--- src/main/java/com/github/mirafi/homemedkit/controller/BotController.java
+++ src/main/java/com/github/mirafi/homemedkit/controller/BotController.java
@@ -1,2 +1,52 @@
-package com.github.mirafi.homemedkit.controller;public class BotController {
+package com.github.mirafi.homemedkit.controller;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+@Component
+public class BotController extends TelegramLongPollingBot {
+
+    private final String username;
+    private final String token;
+
+    public BotController(@Value("${telegram.bot.username}") String username, @Value("${telegram.bot.token}") String token) {
+        this.username = username;
+        this.token = token;
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @SneakyThrows
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            SendMessage.SendMessageBuilder sendMessageBuilder = SendMessage.builder().chatId(message.getChatId().toString());
+            if (message.getText().trim().toLowerCase(Locale.ROOT).equals("ping")) {
+                sendMessageBuilder.text("Pong");
+            } else {
+                sendMessageBuilder.text("Echo! " + message.getText());
+            }
+            execute(sendMessageBuilder.build());
+        }
+    }
+
+
 }
